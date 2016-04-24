d82657a212fc767685a6b671cf2a9506fda55825
greenglass <greenglasscode@gmail.com>
2016-04-24 18:17:17 +0000

    touch up and added gems for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # TODO : set this to actual host
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://overseer-cleanup.herokuapp.com/' }
 end
