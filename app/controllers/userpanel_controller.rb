71f923c86d7ca1539828f8efd63a9f6e7486eb5a
Derek Mardian <greenglasscode@gmail.com>
2016-04-24 22:45:08 -0700

    base for user handling down and a few restrictions in place. got content partial initial design in. figured out to use a new controller for each resource being cleaned, so def index can load lists accordingly.

--- app/controllers/userpanel_controller.rb
+++ app/controllers/userpanel_controller.rb
@@ -1,2 +1,10 @@
 class UserpanelController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @all_machines = ['test1', 'test2', 'test3']
+    @delete_machines = ['delete1', 'delete2', 'delete3']
+    @protect_machines = ['protect1', 'protect2', 'protect3']
+    @rules = ['rule1', 'rule2', 'rule3']
+  end
 end
